8fbf515f0f46238ff96eef9679771bb84980d1dc
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2017-09-27 22:59:40 -0500

    Bring in all the diagram assets;
    - new Shortcode
    - Styles
    - JavaScript for initializing

--- static_src/js/main.js
+++ static_src/js/main.js
@@ -1,7 +1,12 @@
 var $ = require( 'jquery' );
+var mermaid = require( 'mermaid' );
 var jQuery = window.jQuery = $;
 window.$ = $;
 require('cloudgov-style');
+
+mermaid.initialize( {
+  startOnLoad: false,
+} );
 
 var anchorJS = require('anchor-js');
 require('jquery.scrollto');
@@ -267,20 +272,32 @@ function initializeJS() {
 	})();
 })(jQuery);
 
+// Right now this only supports a single Mermaid diagram on the page.
 var swimWithTheMermaids = function() {
-
-  // Are there any diagrams here? if not skip.
-  if ( 1 > $( '#js-stage' ).length ) {
+  // Are there any diagrams here?
+  if ( 1 > $( '.js-diagrams' ).length ) {
     return undefined;
   }
 
-  mermaid.mermaidAPI.render(
-    'js-stage',
-    ss.textContent.replace( /\n/g, '\\n' ),
-    function callbackMermaidRender( c ) {
-      ss.innerHTML = c;
-    }
-  );
+  var diagrams = $( '.js-diagrams' );
+  var padding = 30;
+
+  diagrams.each( function ( idx, el ) {
+    var $el = $( el );
+    var diagramText = $( el ).find( '.js-diagrams__raw' ).text()
+    mermaid.render(
+      $el.find( '.js-diagrams__stage' ).attr( 'id' ),
+      diagramText,
+      function callbackMermaidRender( c ) {
+        $el.find( '.js-diagrams__stage' ).html( c );
+      }
+    );
+    $el.find( '.js-diagrams__stage' ).find( 'style' ).remove();
+    var updatedHeight = Math.ceil( $el.find( '.js-diagrams__stage svg .output' )[0].getBBox().height ) + padding
+    var updatedWidth = Math.ceil( $el.find( '.js-diagrams__stage svg .output' )[0].getBBox().width ) + padding
+    $el.find( '.js-diagrams__stage svg' ).height( updatedHeight );
+    $el.find( '.js-diagrams__stage' ).width( updatedWidth );
+  } );
 
 }
 
