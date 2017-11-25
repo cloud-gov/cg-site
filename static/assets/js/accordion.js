fb4e89f3456162cd40dc828088b71076015f8c75
femmebot <femmebot@gmail.com>
2017-11-25 01:38:42 -0500

    fixed accordion height

--- static/assets/js/accordion.js
+++ static/assets/js/accordion.js
@@ -7,14 +7,14 @@ $( document ).ready(function() {
   $( ".accordion-container section:first-of-type article" ).show();
   $( ".accordion-container section:first-of-type" ).css( "background-color", "#e7e9eb" );
   if ( $(window).width() > 959 ) {
-    $( ".accordion-container section:not(:first-of-type) .option" ).css( "height", accordionHeight - defaultHeight );
+    $( ".accordion-container section:not(:first-of-type) .option" ).css( "height", accordionHeight );
   }
 
   $( ".accordion-container .option-input" ).click(function() {
     $( ".accordion-container article" ).hide();
     $( ".accordion-container section" ).css( "background-color", "#d3d9df" );
     if ( $(window).width() > 959 ) {
-      $( ".accordion-container section .option" ).css( "height", accordionHeight - defaultHeight );
+      $( ".accordion-container section .option" ).css( "height", accordionHeight );
     }
     $( this ).parent().siblings( ".accordion-container article" ).delay( 400 ).fadeIn( 400 );
     $( this ).parent().parent( ".accordion-container section" ).css( "background-color", "#e7e9eb" ).fadeIn();
