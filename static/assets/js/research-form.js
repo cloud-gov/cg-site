9119923b904fb621a3f047b8b61014b6a865014f
femmebot <femmebot@gmail.com>
2018-02-26 14:01:32 -0500

    added recruiter to select pricing pages

--- static/assets/js/research-form.js
+++ static/assets/js/research-form.js
@@ -1,10 +1,8 @@
 $( document ).ready(function() {
 
   $( ".form-container" ).hide();
-  // $( ".form-container" ).css( "display", "none" );
 
   $( ".handle-button" ).click(function() {
-    // $( ".form-container" ).css( "display", "block" );
     $( ".form-container" ).toggle();
   });
 
