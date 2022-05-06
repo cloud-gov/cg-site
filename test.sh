29c5e13659575b82006c097fdedecf0cd8d48fcc
Mark Boyd <mark.boyd@gsa.gov>
2022-05-06 14:15:52 -0400

    refactor script

--- test.sh
+++ test.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-LYCHEE_ARGS="--github-token fake"
+LYCHEE_ARGS="--github-token $GITHUB_TOKEN"
 LYCHEE_ARGS="${LYCHEE_ARGS} --max-concurrency 64"
 LYCHEE_ARGS="${LYCHEE_ARGS} --verbose"
 LYCHEE_ARGS="${LYCHEE_ARGS} --no-progress"
 LYCHEE_ARGS="${LYCHEE_ARGS} './*.md' './_site/**/*.html'"
 
-HEAD_SHA=$(git rev-parse HEAD)
-ADDED_FILES=$(git diff --name-only --diff-filter A master ${HEAD_SHA})
-
 ADDED_KBARTICLES=$(echo "${ADDED_FILES}" | grep _kbarticles | xargs basename | cut -f 1 -d '.')
 for article in $ADDED_KBARTICLES; do
-    echo "$article"
+    echo "addding $article to be excluded by link checker"
+    LYCHEE_ARGS="${LYCHEE_ARGS} --exclude ${article}"
 done
+
+echo "lychee args: ${LYCHEE_ARGS}"
+
+if [ -n "${GITHUB_ENV}" ]; then
+    echo "LYCHEE_ARGS=${LYCHEE_ARGS}" >> "${GITHUB_ENV}"
+fi
