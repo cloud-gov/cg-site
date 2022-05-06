95db17a24788c70fdd4952fc6f68f40d6d4c63d4
Mark Boyd <mark.boyd@gsa.gov>
2022-05-06 14:36:39 -0400

    refactor script and workflow

--- scripts/add-lychee-excludes.sh
+++ scripts/add-lychee-excludes.sh
@@ -5,6 +5,7 @@ LYCHEE_ARGS="${LYCHEE_ARGS} --max-concurrency 64"
 LYCHEE_ARGS="${LYCHEE_ARGS} --verbose"
 LYCHEE_ARGS="${LYCHEE_ARGS} --no-progress"
 
+# exclude new knowledge base articles which haven't been published to the site yet
 ADDED_KBARTICLES=$(echo "${ADDED_FILES}" | grep _kbarticles | xargs basename | cut -f 1 -d '.')
 for article in $ADDED_KBARTICLES; do
     LYCHEE_ARGS="${LYCHEE_ARGS} --exclude ${article}"
