a0e633bca9fcca893cce524b5fd0df1d60826cac
Mark Boyd <mark.boyd@gsa.gov>
2022-05-06 14:30:13 -0400

    refactor script

--- scripts/add-lychee-excludes.sh
+++ scripts/add-lychee-excludes.sh
@@ -4,11 +4,12 @@ LYCHEE_ARGS="--github-token $GITHUB_TOKEN"
 LYCHEE_ARGS="${LYCHEE_ARGS} --max-concurrency 64"
 LYCHEE_ARGS="${LYCHEE_ARGS} --verbose"
 LYCHEE_ARGS="${LYCHEE_ARGS} --no-progress"
-LYCHEE_ARGS="${LYCHEE_ARGS} './*.md' './_site/**/*.html'"
 
 ADDED_KBARTICLES=$(echo "${ADDED_FILES}" | grep _kbarticles | xargs basename | cut -f 1 -d '.')
 for article in $ADDED_KBARTICLES; do
     LYCHEE_ARGS="${LYCHEE_ARGS} --exclude ${article}"
 done
+
+LYCHEE_ARGS="${LYCHEE_ARGS} './*.md' './_site/**/*.html'"
 
 echo "$LYCHEE_ARGS"
