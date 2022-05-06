8d199fd403fade3c46f9f2c4d9513818ca24998a
Mark Boyd <mark.boyd@gsa.gov>
2022-05-06 15:37:03 -0400

    refactor script

--- scripts/add-lychee-excludes.sh
+++ scripts/add-lychee-excludes.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-LYCHEE_ARGS="--github-token $GITHUB_TOKEN"
-LYCHEE_ARGS="${LYCHEE_ARGS} --max-concurrency 64"
-LYCHEE_ARGS="${LYCHEE_ARGS} --verbose"
-LYCHEE_ARGS="${LYCHEE_ARGS} --no-progress"
-LYCHEE_ARGS="${LYCHEE_ARGS} './*.md' './_site/**/*.html'"
+LYCHEE_EXCLUDES=""
+if [ -z "${ADDED_FILES}" ]; then
+    ADDED_FILES=$(git diff --name-only --diff-filter A master "$(git rev-parse HEAD)")
+fi
 
 # exclude new knowledge base articles which haven't been published to the site yet
 ADDED_KBARTICLES=$(echo "${ADDED_FILES}" | grep _kbarticles | xargs basename | cut -f 1 -d '.')
 for article in $ADDED_KBARTICLES; do
-    LYCHEE_ARGS="${LYCHEE_ARGS} --exclude ${article}"
+    LYCHEE_EXCLUDES="${LYCHEE_EXCLUDES} --exclude ${article}"
 done
 
-echo "$LYCHEE_ARGS"
+echo "$LYCHEE_EXCLUDES"
