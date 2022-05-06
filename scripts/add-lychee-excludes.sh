fb4c418b1fcc34bc313e5f3f598e8ed9edd70c2b
Mark Boyd <mark.boyd@gsa.gov>
2022-05-06 14:26:14 -0400

    refactor workflow

--- scripts/add-lychee-excludes.sh
+++ scripts/add-lychee-excludes.sh
@@ -8,7 +8,6 @@ LYCHEE_ARGS="${LYCHEE_ARGS} './*.md' './_site/**/*.html'"
 
 ADDED_KBARTICLES=$(echo "${ADDED_FILES}" | grep _kbarticles | xargs basename | cut -f 1 -d '.')
 for article in $ADDED_KBARTICLES; do
-    echo "addding $article to be excluded by link checker"
     LYCHEE_ARGS="${LYCHEE_ARGS} --exclude ${article}"
 done
 
