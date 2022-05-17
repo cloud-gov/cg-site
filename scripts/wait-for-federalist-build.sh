ec4249bc970bee35ce0b74e640f6793db6404ff7
Mark Boyd <mark.boyd@gsa.gov>
2022-05-17 10:17:28 -0400

    fix script

--- scripts/wait-for-federalist-build.sh
+++ scripts/wait-for-federalist-build.sh
@@ -12,20 +12,20 @@ fi
 
 # Query for Federalist build success and return the preview URL if successful
 is_federalist_build_successful() {
-    curl --silent "$GITHUB_API_URL/repos/${GITHUB_REPO}/commits/$GITHUB_SHA/status" \
+    curl --silent "$GITHUB_API_URL/repos/${GITHUB_REPOSITORY}/commits/$GITHUB_SHA/status" \
         | jq --exit-status -r -c '.statuses[] | select(.context | contains("federalist/build")) | select(.state | contains("success")) | .target_url'
 }
 
-# attempt_counter=0
-# max_attempts=20
+attempt_counter=0
+max_attempts=20
 
-# while ! is_federalist_build_successful; do
-#     if [ ${attempt_counter} -eq ${max_attempts} ]; then
-#       echo "Max attempts reached"
-#       exit 1
-#     fi
+while ! is_federalist_build_successful; do
+    if [ ${attempt_counter} -eq ${max_attempts} ]; then
+      echo "Max attempts reached"
+      exit 1
+    fi
 
-#     echo "Did not find successful Federalist build, retrying"
-#     attempt_counter=$((attempt_counter+1))
-#     sleep 30
-# done
+    echo "Did not find successful Federalist build, retrying"
+    attempt_counter=$((attempt_counter+1))
+    sleep 30
+done
