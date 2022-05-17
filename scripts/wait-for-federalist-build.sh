79012347d15021d3c834c445e04ad1581903fd37
Mark Boyd <mark.boyd@gsa.gov>
2022-05-17 10:08:33 -0400

    test script update

--- scripts/wait-for-federalist-build.sh
+++ scripts/wait-for-federalist-build.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-GITHUB_REPO=$1
-if [ -z "$GITHUB_REPO" ]; then
+if [ -z "$GITHUB_REPOSITORY" ]; then
     echo "Github repository information is required as first argument"
     exit 1
 fi
 
-GITHUB_SHA=$2
 if [ -z "$GITHUB_SHA" ]; then
     echo "Commit SHA is required as second argument"
     exit 1
@@ -18,16 +16,16 @@ is_federalist_build_successful() {
         | jq --exit-status -r -c '.statuses[] | select(.context | contains("federalist/build")) | select(.state | contains("success")) | .target_url'
 }
 
-attempt_counter=0
-max_attempts=20
+# attempt_counter=0
+# max_attempts=20
 
-while ! is_federalist_build_successful; do
-    if [ ${attempt_counter} -eq ${max_attempts} ]; then
-      echo "Max attempts reached"
-      exit 1
-    fi
+# while ! is_federalist_build_successful; do
+#     if [ ${attempt_counter} -eq ${max_attempts} ]; then
+#       echo "Max attempts reached"
+#       exit 1
+#     fi
 
-    echo "Did not find successful Federalist build, retrying"
-    attempt_counter=$((attempt_counter+1))
-    sleep 30
-done
+#     echo "Did not find successful Federalist build, retrying"
+#     attempt_counter=$((attempt_counter+1))
+#     sleep 30
+# done
