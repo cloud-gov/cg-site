7d05036f59da39eeef206a85e2c53033c7c72820
Mark Boyd <mark.boyd@gsa.gov>
2022-05-17 11:14:46 -0400

    update workflow and script

--- scripts/wait-for-federalist-build.sh
+++ scripts/wait-for-federalist-build.sh
@@ -1,19 +1,40 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ -z "$GITHUB_API_URL" ]; then
+    echo "GITHUB_API_URL is required as environment variable" >&2
+    exit 1
+fi
 
 if [ -z "$GITHUB_REPOSITORY" ]; then
-    echo "Github repository information is required as first argument"
+    echo "GITHUB_REPOSITORY information is required as environment variable" >&2
     exit 1
 fi
 
 if [ -z "$GITHUB_SHA" ]; then
-    echo "Commit SHA is required as second argument"
+    echo "GITHUB_SHA is required as environment variable" >&2
     exit 1
 fi
 
+debug() {
+	if [[ -n ${DEBUG} && ${DEBUG} != '0' ]];
+		then echo >&2 '>> ' "$*"
+	fi
+}
+
 # Query for Federalist build success and return the preview URL if successful
 is_federalist_build_successful() {
-    curl --silent "$GITHUB_API_URL/repos/${GITHUB_REPOSITORY}/commits/$GITHUB_SHA/status" \
-        | jq --exit-status -r -c '.statuses[] | select(.context | contains("federalist/build")) | select(.state | contains("success")) | .target_url'
+    BUILD_INFO=$(curl --silent "$GITHUB_API_URL/repos/${GITHUB_REPOSITORY}/commits/$GITHUB_SHA/status" \
+        | jq --exit-status -c '.statuses[] | select(.context | contains("federalist/build"))')
+
+    BUILD_STATE=$(echo "$BUILD_INFO" | jq -r '.state')
+    debug "build state: $BUILD_STATE"
+    
+    if [ "$BUILD_STATE" = "success" ]; then
+        echo "$BUILD_INFO" | jq -r '.target_url'
+        exit 0
+    fi
+
+    exit 1
 }
 
 attempt_counter=0
@@ -21,11 +42,11 @@ max_attempts=20
 
 while ! is_federalist_build_successful; do
     if [ ${attempt_counter} -eq ${max_attempts} ]; then
-      echo "Max attempts reached"
+      debug "Max attempts reached"
       exit 1
     fi
 
-    echo "Did not find successful Federalist build, retrying"
+    debug "Did not find successful Federalist build, retrying"
     attempt_counter=$((attempt_counter+1))
     sleep 30
 done
